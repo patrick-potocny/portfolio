b7871c44b3dc4dfe80c9e88cc6eb0d6d5192e158
patrick-potocny <patrik.potocny85@gmail.com>
2023-03-21 00:12:02 +0100

    Contact Page

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -2,6 +2,13 @@ import Layout from "@/components/Layout";
 import { SeenWelcomeScreenCtx } from "@/context/SeenWelcomePageCtx";
 import Head from "next/head";
 import React, { useContext, useEffect } from "react";
+import styles from "@/styles/pages/Contact.module.scss";
+import Image from "next/image";
+import email from "@/public/images/email.svg";
+import linkedIn from "@/public/images/linkedIn.svg";
+import phone from "@/public/images/phone.svg";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import { motion } from "framer-motion";
 
 export default function Contact() {
   const { setSeenWelcomePage } = useContext(SeenWelcomeScreenCtx);
@@ -17,7 +24,58 @@ export default function Contact() {
       <Head>
         <title>Contact</title>
       </Head>
-      <h1>COntact</h1>
+      <motion.h1
+        initial={{ y: -20, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ duration: 0.6 }}
+        className={styles.title}
+      >
+        Get in touch
+      </motion.h1>
+      <motion.div className={styles.container}>
+        <motion.div
+          initial={{ x: 20, opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{ duration: 1 }}
+          className={styles.contactCard}
+        >
+          <div className={styles.leftCorners} />
+          <div className={styles.rightCorners} />
+          <Image className={styles.icon} src={email} alt="Email" />
+          <h2 className={styles.title}>Email</h2>
+          <p className={styles.contact}>email@email.com</p>
+          <CopyToClipboard text="email@email.com">
+            <button className={styles.btn}>Copy Email</button>
+          </CopyToClipboard>
+        </motion.div>
+        <motion.div
+          initial={{ x: 20, opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{ delay: 0.3, duration: 1 }}
+          className={styles.contactCard}
+        >
+          <div className={styles.leftCorners} />
+          <div className={styles.rightCorners} />
+          <Image className={styles.icon} src={linkedIn} alt="LinkedIn" />
+          <h2 className={styles.title}>LinkedIn</h2>
+          <button className={styles.btn}>My Profile</button>
+        </motion.div>
+        <motion.div
+          initial={{ x: 20, opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{ delay: 0.6, duration: 1 }}
+          className={styles.contactCard}
+        >
+          <div className={styles.leftCorners} />
+          <div className={styles.rightCorners} />
+          <Image className={styles.icon} src={phone} alt="Phone" />
+          <h2 className={styles.title}>Phone Number</h2>
+          <p className={styles.contact}>+421 940 116 860</p>
+          <CopyToClipboard text="+421 940 116 860">
+            <button className={styles.btn}>Copy Number</button>
+          </CopyToClipboard>
+        </motion.div>
+      </motion.div>
     </Layout>
   );
 }
